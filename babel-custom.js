7920a19ebad6cc291de2fbeec0506caba3b56c64
Frederic Desmarais <frederic.desmarais@radio-canada.ca>
2022-02-03 16:40:14 -0500

    command: copy only install cli

---
+++ babel-custom.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
+import { argv } from "process"
+import detectIndent from "detect-indent"
+
+import fs from "fs"
+
+/* eslint-disable no-console */
+/* eslint-disable no-useless-escape */
+
+argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`)
+})
+
+const sourcePath = argv[2]
+const action = argv[3]
+console.log({ sourcePath })
+
+const source = fs.readFileSync(`${sourcePath}/babel.config.json`, "utf-8")
+const indent = detectIndent(source).indent || "  "
+let pack = JSON.parse(source)
+console.log({ pack })
+
+export const customBabel = () => {
+  // commandOptions = "--if-present"
+
+  if (action == "add") {
+    // pack.plugins = ["@babel/plugin-transform-react-jsx-source"]
+    // pack.env = {
+
+    // }
+    pack = {
+      ...pack,
+      plugins: ["@babel/plugin-transform-react-jsx-source"],
+      env: {
+        development: {
+          sourceMaps: "inline",
+          retainLines: true,
+        },
+      },
+    }
+  } else if (action == "delete") {
+    delete pack.plugins
+    delete pack.env
+  }
+  fs.writeFileSync(
+    `${sourcePath}/babel.config.json`,
+    JSON.stringify(pack, null, indent)
+  )
+}
+
+customBabel()
--- beta-syntax-generator.js
+++ beta-syntax-generator.js
@@ -1,154 +1,168 @@
 /* eslint-disable no-undef */
 /* eslint-disable require-jsdoc */
 /* eslint-disable max-len */
-import clipboard from 'clipboardy';
-import chalk from 'chalk';
-import { argv } from 'process';
-import detectIndent from 'detect-indent';
+import clipboard from "clipboardy"
+import chalk from "chalk"
+import { argv } from "process"
+import detectIndent from "detect-indent"
 
-import pkg from 'enquirer';
-const { Select, prompt } = pkg;
+import pkg from "enquirer"
+const { Select, prompt } = pkg
 
-import fs from 'fs';
-import { execSync } from 'child_process';
+import fs from "fs"
+import { execSync } from "child_process"
 
 /* eslint-disable no-console */
 /* eslint-disable no-useless-escape */
 
-
 export const betaSyntaxPublish = () => {
-  // commandOptions = "--if-present"
-
   argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-  });
+    console.log(`${index}: ${val}`)
+  })
 
-  const gitBranch = argv[3];
+  const gitBranch = argv[3]
+  const sourcePath = argv[2]
 
+  console.log({ gitBranch })
+  console.log({ sourcePath })
 
-  const sourcePath = argv[2];
+  const source = fs.readFileSync(`${sourcePath}/package.json`, "utf-8")
+  const indent = detectIndent(source).indent || "  "
+  const pack = JSON.parse(source)
+  const { version } = pack
 
-  console.log({ gitBranch });
-  console.log({ sourcePath });
-
-  const source = fs.readFileSync(`${sourcePath}/package.json`, 'utf-8');
-  const indent = detectIndent(source).indent || '  ';
-  const pack = JSON.parse(source);
-  const { version, scripts } = pack;
-
-  const preparePublishing = ['prepublish', 'prepublishOnly', 'type-check'].map(function (e) {
-    return `npm run ${e} --if-present && `;
-  });
-  console.log(chalk.red(preparePublishing.join('')));
+  const preparePublishing = ["prepublish", "prepublishOnly", "type-check"].map(
+    function (e) {
+      return `npm run ${e} --if-present && `
+    }
+  )
+  console.log(chalk.red(preparePublishing.join("")))
   // eslint-disable-next-line max-len
   // Test si le nom de la branch contient d'association au ticket jira ie:"MNMN-1234"
-  const regexGitBranchWorkItem = /\/[A-Za-z]+[-_]?[0-9]+/;
+  const regexGitBranchWorkItem = /\/[A-Za-z]+[-_]?[0-9]+/
 
-  const neon = chalk.cyanBright;
-  const success = chalk.greenBright;
-  // 🤖 Syntaxeur de version beta
-  // 📖 [Documentation](https://cbcradiocanada.atlassian.net/wiki/spaces/RCA/pages/1821638888/Artifacts+-+Mode+d+emploi#Script-%3A-Syntaxeur-de-version-beta-%2B-autopublishing)
+  const neon = chalk.cyanBright
+  const success = chalk.greenBright
 
   if (regexGitBranchWorkItem.test(gitBranch)) {
-    console.log(neon(`
+    console.log(
+      neon(`
 ╔╗ ┌─┐┌┬┐┌─┐  ╦  ╦┌─┐┬─┐┌─┐┬┌─┐┌┐┌  ╔═╗┌─┐┌┐┌┌─┐┬─┐┌─┐┌┬┐┌─┐┬─┐
 ╠╩╗├┤  │ ├─┤  ╚╗╔╝├┤ ├┬┘└─┐││ ││││  ║ ╦├┤ │││├┤ ├┬┘├─┤ │ │ │├┬┘
 ╚═╝└─┘ ┴ ┴ ┴   ╚╝ └─┘┴└─└─┘┴└─┘┘└┘  ╚═╝└─┘┘└┘└─┘┴└─┴ ┴ ┴ └─┘┴└─
 Current branch name:
 ${gitBranch}
-`));
+`)
+    )
   } else {
     console.error(
-      `⛔️ La syntaxe de la branche "${gitBranch}" doit contenir l'ID d'association au ticket jira (ie:"MNMN-1234")`,
-    );
-    process.exit(1);
+      `⛔️ La syntaxe de la branche "${gitBranch}" doit contenir l'ID d'association au ticket jira (ie:"MNMN-1234")`
+    )
+    process.exit(1)
   }
 
-
   const beta = {
-    pre: '0.0.0-beta-from',
-    fromVersion: '',
-    workItem: regexGitBranchWorkItem.exec(gitBranch)[0].replaceAll(/[\/]/gm, ''),
-    betaVersion: version.split('.').at(-1),
-  };
-  console.log(`From {"Version" : "${pack.version}"}`);
+    pre: "0.0.0-beta-from",
+    fromVersion: "",
+    workItem: regexGitBranchWorkItem
+      .exec(gitBranch)[0]
+      .replaceAll(/[\/]/gm, ""),
+    betaVersion: version.split(".").at(-1),
+  }
+  console.log(`From {"Version" : "${pack.version}"}`)
 
   // TODO : Évaluer si une version beta a déjà été publié sous `${pack.name}@${pack.version}`. Si oui mettre a jour `pack.version`dans package.json pour matcher la dernière version publié, continuer.
 
-  const isBeta = version.startsWith(beta.pre);
+  const isBeta = version.startsWith(beta.pre)
 
   if (!isBeta) {
-    beta.fromVersion = pack.version.replaceAll('.', '');
-    pack.version = `${beta.pre}${beta.fromVersion}-${beta.workItem}.0`;
+    beta.fromVersion = pack.version.replaceAll(".", "")
+    pack.version = `${beta.pre}${beta.fromVersion}-${beta.workItem}.0`
   } else {
-    const re = /(?!0.0.0-beta-from)[0-9]{2,}/;
-    beta.fromVersion = re.exec(version);
-    pack.version = `${beta.pre}${beta.fromVersion}-${beta.workItem}.${Number(beta.betaVersion) + 1
-      }`;
+    const re = /(?!0.0.0-beta-from)[0-9]{2,}/
+    beta.fromVersion = re.exec(version)
+    pack.version = `${beta.pre}${beta.fromVersion}-${beta.workItem}.${
+      Number(beta.betaVersion) + 1
+    }`
   }
-  fs.writeFileSync(`${sourcePath}/package.json`, JSON.stringify(pack, null, indent));
+  fs.writeFileSync(
+    `${sourcePath}/package.json`,
+    JSON.stringify(pack, null, indent)
+  )
 
-  /* console.log(`
-  --------------------------------
-  >>>>>>> Publishing 🗞 using "npm publish --tag beta":
-  ${pack.name}@${pack.version}
-  --------------------------------
-  `); */
-
-  initPublish();
+  initPublish()
 
   async function initPublish() {
     const choices = [
-      { message: `Publier ${pack.name}@${pack.version}`, name: 'publier' },
-      { message: `Renommer la branche ${gitBranch}`, name: 'corriger' },
-    ];
+      { message: `Publier ${pack.name}@${pack.version}`, name: "publier" },
+      {
+        message: `Copier la commande pour installer ${pack.name}@${pack.version}`,
+        name: "install",
+      },
+      { message: `Renommer la branche ${gitBranch}`, name: "corriger" },
+    ]
 
     const prompt = new Select({
-      type: 'select',
+      type: "select",
       message: `Que voulez vous faire ?`,
       initial: 0,
-      format: () => '',
+      format: () => "",
       choices,
-    });
+    })
 
     try {
-      const choice = await prompt.run();
-      startFromChoice(choice);
+      const choice = await prompt.run()
+      startFromChoice(choice)
     } catch {
-      process.exit(0);
+      process.exit(0)
     }
+  }
+  async function copyInstallCommand() {
+    const installCommand = `npm i ${pack.name}@${pack.version} --save-exact`
+
+    console.log(`>>>>>>>>>>>>>>> Installez la beta publié avec:`)
+    console.log(success(installCommand))
+    clipboard.writeSync(installCommand)
   }
 
   async function startFromChoice(choice) {
-    if (choice === 'publier') {
-      const publishCommand = `cd ${sourcePath} ${preparePublishing.join('')} npm publish --tag beta`
-      console.log(success(publishCommand));
-      execSync(publishCommand, { encoding: 'utf-8' });
-      // console.log("first");
-      console.log(`>>>>>>>>>>>>>>> Installez la beta publié avec:`);
-      const installCommand = `npm i ${pack.name}@${pack.version} --save-exact`;
-      console.log(success(installCommand));
-      clipboard.writeSync(installCommand);
-      // clipboard.readSync();
-    } else if (choice === 'corriger') {
-      process.exit(0);
-      const currentName = gitBranch;
+    if (choice === "publier") {
+      const publishCommand = `cd ${sourcePath} ${preparePublishing.join(
+        ""
+      )} npm publish --tag beta`
+      console.log(success(publishCommand))
+      execSync(publishCommand, { encoding: "utf-8" })
+
+      copyInstallCommand()
+    } else if (choice === "install") {
+      pack.version = `${beta.pre}${beta.fromVersion}-${beta.workItem}.${
+        Number(beta.betaVersion) - 1
+      }`
+      fs.writeFileSync(
+        `${sourcePath}/package.json`,
+        JSON.stringify(pack, null, indent)
+      )
+
+      copyInstallCommand()
+    } else if (choice === "corriger") {
+      // process.exit(0);
+      const currentName = gitBranch
 
       const newName = await prompt({
-        type: 'input',
-        name: 'branch',
+        type: "input",
+        name: "branch",
         message: `
-ancien  :   ${currentName}
-nouveau :`,
-      });
+  ancien  :   ${currentName}
+  nouveau :`,
+      })
 
-      console.log(newName.branch);
-      console.log('same work item?', (newName.branch.includes(beta.workItem)));
-      process.exit(0);
+      console.log(newName.branch)
+      console.log("same work item?", newName.branch.includes(beta.workItem))
+      process.exit(0)
       // execSync(`git branch -m ${newName.input}`, { encoding: "utf-8" });
-      initPublish();
+      initPublish()
     }
   }
-};
+}
 
-betaSyntaxPublish();
+betaSyntaxPublish()
