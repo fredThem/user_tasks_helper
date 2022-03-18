072d7f1fe07e3ff9e7be88d40f71a27621a38217
Frederic Desmarais <frederic.desmarais@radio-canada.ca>
2022-03-17 22:10:13 -0400

    up

--- snippets/debug/mutationObserver.js
+++ snippets/debug/mutationObserver.js
@@ -5,39 +5,39 @@
  * @see [developer.mozilla.org/.../API/MutationObserver/observe](https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe)
  */
 const Observing = (targetClass, config) => {
-  const elem = document.getElementsByClassName(targetClass)[0]
-  // console.log(elem);
-  // console.debug(elem);
-  let observer = new MutationObserver((mutationRecords) => {
-    console.group(`${targetClass} mutations`)
-    for (const mutationRecord in mutationRecords) {
-      if (Object.hasOwnProperty.call(mutationRecords, mutationRecord)) {
-        const record = mutationRecords[mutationRecord]
-        const oldValue = record.oldValue.split(" ")
-        const newValue = record.target.className.split(" ")
-        const diff = {
-          intersection: oldValue.filter((x) => newValue.includes(x)),
-          removed: oldValue.filter((x) => !newValue.includes(x)),
-          added: newValue.filter((x) => !oldValue.includes(x)),
+  const elements = document.getElementsByClassName(targetClass)
+  Array.prototype.forEach.call(elements, function (elem) {
+    let observer = new MutationObserver((mutationRecords) => {
+      console.group(`${targetClass} mutations`)
+      for (const mutationRecord in mutationRecords) {
+        if (Object.hasOwnProperty.call(mutationRecords, mutationRecord)) {
+          const record = mutationRecords[mutationRecord]
+          const oldValue = record.oldValue.split(" ")
+          const newValue = record.target.className.split(" ")
+          const diff = {
+            intersection: oldValue.filter((x) => newValue.includes(x)),
+            removed: oldValue.filter((x) => !newValue.includes(x)),
+            added: newValue.filter((x) => !oldValue.includes(x)),
+          }
+          console.table(diff)
+          // const transit = { "oldValue": oldValue, "newValue": newValue}
+          // console.table(transit);
         }
-        console.table(diff)
-        // const transit = { "oldValue": oldValue, "newValue": newValue}
-        // console.table(transit);
       }
-    }
-    console.groupEnd(`${targetClass} mutations`)
-    console.table(mutationRecords) // console.log(the changes)
+      console.groupEnd(`${targetClass} mutations`)
+      console.table(mutationRecords) // console.log(the changes)
+    })
+
+    console.group("config")
+
+    console.warn(`observing attribute "${config.attributeFilter}" on:`, elem)
+    console.log({ config })
+    observer.observe(elem, config)
+    console.log(observer)
+
+    console.groupEnd("config")
   })
   // observe everything except attributes
-
-  console.group("config")
-
-  console.warn(`observe ${config.attributeFilter} on:`, elem)
-  console.log({ config })
-  observer.observe(elem, config)
-  console.log(observer)
-
-  console.groupEnd("config")
 }
 
 Observing("search-filter", {
